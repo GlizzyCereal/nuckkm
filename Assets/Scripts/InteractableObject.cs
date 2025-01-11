79f409efe6b1af0bef06cea8fbab2ada646d22a7
GlizzyCereal <haroldas.bogdanovicius@gmail.com>
2025-01-11 23:46:54 +0200

    picking up item mimic

--- Assets/Scripts/InteractableObject.cs
+++ Assets/Scripts/InteractableObject.cs
@@ -1,13 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
- 
 public class InteractableObject : MonoBehaviour
 {
-    public string ItemName;
+    private bool playerInRange;
+    public bool PlayerInRange
+    {
+        get { return playerInRange; }
+        set { playerInRange = value; }
+    }
+    public string itemName;
  
     public string GetItemName()
     {
-        return ItemName;
+        return itemName;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = true;
+            PlayerInRange = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = false;
+            PlayerInRange = false;
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0) && playerInRange && SelectionManager.instance != null && SelectionManager.instance.onTarget)
+        {
+            #if UNITY_EDITOR || DEVELOPMENT_BUILD
+                        Debug.Log("Interacting with " + itemName);
+            #endif
+        }
     }
 }
--- Assets/Scripts/SelectionManager.cs
+++ Assets/Scripts/SelectionManager.cs
@@ -7,15 +7,28 @@ using TMPro;
  
 public class SelectionManager : MonoBehaviour
 {
- 
+    public static SelectionManager instance { get; set; }
+    public bool onTarget;
     public GameObject interaction_Info_UI;
     TMP_Text interaction_text;
- 
+
+    void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            instance = this;
+        }
+    }
     private void Start()
     {
+        onTarget = false;
         interaction_text = interaction_Info_UI.GetComponent<TMP_Text>();
     }
- 
+
     void Update()
     {
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
@@ -23,20 +36,23 @@ public class SelectionManager : MonoBehaviour
         if (Physics.Raycast(ray, out hit))
         {
             var selectionTransform = hit.transform;
- 
-            if (selectionTransform.GetComponent<InteractableObject>())
+            InteractableObject interactableObject = selectionTransform.GetComponent<InteractableObject>();
+            if (interactableObject && interactableObject.PlayerInRange)
             {
-                interaction_text.text = selectionTransform.GetComponent<InteractableObject>().GetItemName();
+                onTarget = true;
+                interaction_text.text = interactableObject.GetItemName();
                 interaction_Info_UI.SetActive(true);
             }
             else 
             { 
                 interaction_Info_UI.SetActive(false);
+                onTarget = false;
             }
         }
         else 
         { 
             interaction_Info_UI.SetActive(false);
+            onTarget = false;
         }
     }
 }
